b7b52d9d805ec77ce3659c7dd118c322e4e54d15
Marin Peric Radan <marinpericradan1@gmail.com>
2024-02-14 07:36:40 +0100

    Added basic server setup

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/MPRaiden/RSS-Feed
 go 1.21.3
 
 require (
+	github.com/go-chi/chi v1.5.5 // indirect
 	github.com/go-chi/chi/v5 v5.0.11 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/rs/cors v1.10.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
 	"log"
+	"net/http"
 	"os"
-)
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 func main () {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Coul not load env file")
 	}
 
-	jwtSecret := os.Getenv("PORT")
-	if jwtSecret == "" {
+	port := os.Getenv("PORT")
+	if port == "" {
 		log.Fatal("Unable to load PORT env variable.")
 	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, 
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerError)
+
+	router.Mount("/v1", v1Router)
+	
+	server := &http.Server{
+		Addr: ":" + port,
+		Handler: router,
+	}
+
+	log.Printf("Server is up and running on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 }
